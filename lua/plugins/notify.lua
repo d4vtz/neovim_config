a3e45bdff25342e687149e636baedda123932e39
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-08-31 22:59:25 -0600

    feat: improve notification behavior and controls

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,24 +1,58 @@
 return {
-    {
-        "rcarriga/nvim-notify",
+	{
+		"rcarriga/nvim-notify",
 
-        lazy = false,
+		lazy = false,
 
-        opts = {
-            stages = "fade_in_slide_out",
-            timeout = 2000,
-            background_colour = "#000000",
-            render = "default",
-            max_width = 50,
-            max_height = 10,
-        },
+		keys = {
+			{
+				"<leader>nd",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Descartar notificaciones",
+			},
+			{
+				"<leader>nh",
+				"<cmd>Notifications<CR>",
+				desc = "Historial de notificaciones",
+			},
+		},
 
-        config = function(_, opts)
-            local notify = require("notify")
+		opts = {
+			stages = "fade_in_slide_out",
+			timeout = 2000,
+			background_colour = "#000000",
+			render = "compact",
+			top_down = false,
+			max_width = function()
+				return math.min(80, math.floor(vim.o.columns * 0.5))
+			end,
+			max_height = function()
+				return math.min(15, math.floor(vim.o.lines * 0.4))
+			end,
+		},
 
-            notify.setup(opts)
+		config = function(_, opts)
+			local notify = require("notify")
 
-            vim.notify = notify
-        end,
-    },
+			notify.setup(opts)
+
+			vim.notify = function(message, level, notify_opts)
+				local options = vim.tbl_extend("force", {}, notify_opts or {})
+
+				if options.timeout == nil then
+					if level == vim.log.levels.ERROR then
+						options.timeout = 5000
+					elseif level == vim.log.levels.WARN then
+						options.timeout = 3500
+					else
+						options.timeout = 2000
+					end
+				end
+
+				return notify(message, level, options)
+			end
+		end,
+	},
 }
