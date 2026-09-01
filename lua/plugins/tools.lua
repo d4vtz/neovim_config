90cbe98126f4b0f13c8cc07b92e0cf6c476f8ab4
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-08-31 22:13:50 -0600

    feat: manage formatters with Mason

---
+++ lua/plugins/tools.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        event = "VeryLazy",
+
+        dependencies = {
+            "mason-org/mason.nvim",
+        },
+
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "ruff",
+                "clang-format",
+                "latexindent",
+            },
+
+            auto_update = false,
+            run_on_start = true,
+            start_delay = 3000,
+            debounce_hours = 24,
+
+            integrations = {
+                ["mason-lspconfig"] = false,
+                ["mason-null-ls"] = false,
+                ["mason-nvim-dap"] = false,
+            },
+        },
+    },
+}
