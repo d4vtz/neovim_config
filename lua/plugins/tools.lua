7baf220b83058c5da3e18779531e9bdb37d9713e
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-01 08:08:16 -0600

    build: install shell and latex linters with mason

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,47 +1,49 @@
 return {
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        lazy = true,
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = true,
 
-        cmd = {
-            "MasonToolsInstall",
-            "MasonToolsInstallSync",
-            "MasonToolsUpdate",
-            "MasonToolsUpdateSync",
-            "MasonToolsClean",
-        },
+		cmd = {
+			"MasonToolsInstall",
+			"MasonToolsInstallSync",
+			"MasonToolsUpdate",
+			"MasonToolsUpdateSync",
+			"MasonToolsClean",
+		},
 
-        init = function()
-            vim.defer_fn(function()
-                require("lazy").load({
-                    plugins = {
-                        "mason-tool-installer.nvim",
-                    },
-                })
-            end, 3000)
-        end,
+		init = function()
+			vim.defer_fn(function()
+				require("lazy").load({
+					plugins = {
+						"mason-tool-installer.nvim",
+					},
+				})
+			end, 3000)
+		end,
 
-        dependencies = {
-            "mason-org/mason.nvim",
-        },
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
 
-        opts = {
-            ensure_installed = {
-                "stylua",
-                "ruff",
-                "clang-format",
-            },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"ruff",
+				"clang-format",
+				"shellcheck",
+				"chktex",
+			},
 
-            auto_update = false,
-            run_on_start = true,
-            start_delay = 0,
-            debounce_hours = 24,
+			auto_update = false,
+			run_on_start = true,
+			start_delay = 0,
+			debounce_hours = 24,
 
-            integrations = {
-                ["mason-lspconfig"] = false,
-                ["mason-null-ls"] = false,
-                ["mason-nvim-dap"] = false,
-            },
-        },
-    },
+			integrations = {
+				["mason-lspconfig"] = false,
+				["mason-null-ls"] = false,
+				["mason-nvim-dap"] = false,
+			},
+		},
+	},
 }
