9bbb332bbef334495d09d20f71824531e4822b41
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-01 08:49:55 -0600

    feat: integrate lazydev with blink

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -101,10 +101,21 @@ return {
 
 			sources = {
 				default = {
+					"lazydev",
 					"lsp",
 					"path",
 					"snippets",
 					"buffer",
+				},
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+						enabled = function()
+							return vim.bo.filetype == "lua"
+						end,
+					},
 				},
 			},
 
