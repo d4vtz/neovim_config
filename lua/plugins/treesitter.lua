c3ae86d9914309b588daef6d4e5e042057318142
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-01 08:20:52 -0600

    perf: skip treesitter for large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,44 @@
 local parsers = {
-    "bash",
-    "c",
-    "cpp",
-    "json",
-    "latex",
-    "lua",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "toml",
-    "vim",
-    "vimdoc",
-    "yaml",
+	"bash",
+	"c",
+	"cpp",
+	"json",
+	"latex",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"toml",
+	"vim",
+	"vimdoc",
+	"yaml",
 }
 
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "main",
-        lazy = false,
-        build = ":TSUpdate",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
 
-        config = function()
-            local treesitter = require("nvim-treesitter")
+		config = function()
+			local treesitter = require("nvim-treesitter")
 
-            treesitter.setup()
-            treesitter.install(parsers)
+			treesitter.setup()
+			treesitter.install(parsers)
 
-            local group = vim.api.nvim_create_augroup("UserTreesitter", {
-                clear = true,
-            })
+			local group = vim.api.nvim_create_augroup("UserTreesitter", {
+				clear = true,
+			})
 
-            vim.api.nvim_create_autocmd("FileType", {
-                group = group,
-                callback = function()
-                    pcall(vim.treesitter.start)
-                end,
-            })
-        end,
-    },
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(args)
+					if not vim.b[args.buf].bigfile then
+						pcall(vim.treesitter.start)
+					end
+				end,
+			})
+		end,
+	},
 }
