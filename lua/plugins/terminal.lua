c9e1a75083a1a43b82dabe16c49555fbc1aa7a07
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-08-31 23:08:04 -0600

    feat: improve terminal navigation and layout

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -4,7 +4,9 @@ return {
 		version = "*",
 		cmd = {
 			"ToggleTerm",
+			"ToggleTermToggleAll",
 			"TermExec",
+			"TermSelect",
 		},
 
 		keys = {
@@ -13,30 +15,46 @@ return {
 				"<cmd>ToggleTerm direction=float<CR>",
 				desc = "Terminal flotante",
 			},
-
 			{
 				"<leader>th",
 				"<cmd>ToggleTerm direction=horizontal<CR>",
 				desc = "Terminal horizontal",
 			},
-
 			{
 				"<leader>tv",
 				"<cmd>ToggleTerm direction=vertical<CR>",
 				desc = "Terminal vertical",
 			},
+			{
+				"<leader>ts",
+				"<cmd>TermSelect<CR>",
+				desc = "Seleccionar terminal",
+			},
+			{
+				"<leader>ta",
+				"<cmd>ToggleTermToggleAll<CR>",
+				desc = "Mostrar u ocultar terminales",
+			},
 		},
 
 		opts = {
-			size = 15,
+			size = function(terminal)
+				if terminal.direction == "horizontal" then
+					return math.max(12, math.floor(vim.o.lines * 0.3))
+				end
+
+				if terminal.direction == "vertical" then
+					return math.max(40, math.floor(vim.o.columns * 0.4))
+				end
+			end,
 
 			open_mapping = [[<C-\>]],
 
 			hide_numbers = true,
-
+			autochdir = true,
 			shade_terminals = true,
-
 			start_in_insert = true,
+			auto_scroll = true,
 
 			insert_mappings = true,
 			terminal_mappings = true,
@@ -45,14 +63,30 @@ return {
 			persist_mode = true,
 
 			direction = "float",
-
 			close_on_exit = true,
-
 			shell = vim.o.shell,
 
 			float_opts = {
 				border = "rounded",
+				width = function()
+					return math.max(60, math.floor(vim.o.columns * 0.8))
+				end,
+				height = function()
+					return math.max(15, math.floor(vim.o.lines * 0.8))
+				end,
 				winblend = 0,
+				title_pos = "center",
+			},
+
+			winbar = {
+				enabled = true,
+				name_formatter = function(terminal)
+					return string.format(" Terminal %d: %s ", terminal.id, terminal.name)
+				end,
+			},
+
+			responsiveness = {
+				horizontal_breakpoint = 135,
 			},
 		},
 	},
