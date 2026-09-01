cc345a33acb215cdc0306ccf652f15daca001e64
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-01 08:20:57 -0600

    fix: harden lazy bootstrap and theme fallback

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,42 +1,46 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+if not vim.uv.fs_stat(lazypath) then
+	local output = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+
+	if vim.v.shell_error ~= 0 then
+		error("No se pudo instalar lazy.nvim:\n" .. output)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        {
-            import = "plugins",
-        },
-    },
+	spec = {
+		{
+			import = "plugins",
+		},
+	},
 
-    defaults = {
-        lazy = false,
-    },
+	defaults = {
+		lazy = false,
+	},
 
-    rocks = {
-        enabled = false,
-    },
+	rocks = {
+		enabled = false,
+	},
 
-    install = {
-        colorscheme = { "habamax" },
-    },
+	install = {
+		colorscheme = { "dracula", "habamax" },
+	},
 
-    checker = {
-        enabled = false,
-    },
+	checker = {
+		enabled = false,
+	},
 
-    change_detection = {
-        notify = false,
-    },
+	change_detection = {
+		notify = false,
+	},
 })
